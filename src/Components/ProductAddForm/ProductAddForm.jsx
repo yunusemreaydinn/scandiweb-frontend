1231218fa85fc4c714f42131cb11b20418607a7e
yunusemreaydinn <ea.emreaydn.58@gmail.com>
2023-08-02 16:32:49 +0300

    list and form edited

--- src/Components/ProductAddForm/ProductAddForm.jsx
+++ src/Components/ProductAddForm/ProductAddForm.jsx
@@ -17,7 +17,7 @@ const ProductAddForm = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-<<<<<<< HEAD
+
         if (inputs.productType === '') {
             toast.warn('Please select a product type.', {
                 position: "bottom-right",
@@ -46,155 +46,136 @@ const ProductAddForm = () => {
             } catch (error) {
                 console.error("Error: ", error);
             }
-=======
-        try {
-          const response = await fetch("../../scandiweb-backend/App/Api/add_product.php", {
-            method: 'POST', 
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(inputs) 
-          });
-      
-          if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-          } else {
-            navigate("/");
-          }
-        } catch (error) {
-          console.error("Error: ", error);
->>>>>>> 3e36e85fd82782fbb94c5c92b86beb81bc3f1dbb
+        }}
+
+        const additionalFields = () => {
+            if (inputs.productType === 'book') {
+                return (
+                    <>
+                        <tr>
+                            <td><label htmlFor="weight">Weight (KG) </label></td>
+                            <td><input
+                                type="number"
+                                id="weight"
+                                name="weight"
+                                onChange={handleChange}
+                                required /></td>
+                        </tr>
+                        <tr><td colSpan={2}><strong className='fields'>Please provide the weight of the book.</strong></td></tr>
+                    </>
+                );
+            } else if (inputs.productType === 'dvd') {
+                return (
+                    <>
+                        <tr>
+                            <td><label htmlFor="size">Size (MB) </label></td>
+                            <td><input
+                                type="number"
+                                id="size"
+                                name="size"
+                                onChange={handleChange}
+                                required /></td>
+                        </tr>
+                        <tr><td colSpan={2}><strong className='fields'>Please provide the size(MB) of the DVD.</strong></td></tr>
+                    </>
+                );
+            } else if (inputs.productType === 'furniture') {
+                return (
+                    <>
+                        <tr>
+                            <td><label htmlFor="weight">Height (CM) </label></td>
+                            <td><input
+                                type="number"
+                                id="height"
+                                name="height"
+                                onChange={handleChange}
+                                required /></td>
+                        </tr>
+                        <tr>
+                            <td><label htmlFor="weight">Width (CM) </label></td>
+                            <td><input
+                                type="number"
+                                id="width"
+                                name="width"
+                                onChange={handleChange}
+                                required /></td>
+                        </tr>
+                        <tr>
+                            <td><label htmlFor="weight">Length (CM) </label></td>
+                            <td><input
+                                type="number"
+                                id="length"
+                                name="length"
+                                onChange={handleChange}
+                                required /></td>
+                        </tr>
+                        <tr><td colSpan={2}><strong className='fields'>Please provide the dimensions(CM) of the furniture.</strong></td></tr>
+                    </>
+                );
+            } else { return null; }
         }
+
+        return (
+            <>
+                <div className='form'>
+                    <form id="product_form" method='post' onSubmit={handleSubmit}>
+                        <Header title={"Product Add"} />
+                        <div className='wrapper'>
+                            <table cellSpacing={10} className='product-add-form'>
+                                <tbody>
+                                    <tr>
+                                        <td><label htmlFor="sku">SKU </label></td>
+                                        <td><input
+                                            type="text"
+                                            maxLength="24"
+                                            name="sku"
+                                            id="sku"
+                                            onChange={handleChange}
+                                            required /></td>
+                                    </tr>
+                                    <tr>
+                                        <td><label htmlFor="name">Name </label></td>
+                                        <td><input
+                                            type="text"
+                                            maxLength="24"
+                                            name="name"
+                                            id="name"
+                                            onChange={handleChange}
+                                            required /></td>
+                                    </tr>
+                                    <tr>
+                                        <td><label htmlFor="price">Price ($) </label></td>
+                                        <td><input
+                                            type="number"
+                                            maxLength="24"
+                                            name="price"
+                                            id="price"
+                                            onChange={handleChange}
+                                            required /></td>
+                                    </tr>
+                                    <tr>
+                                        <td><label htmlFor="productType">Type Switcher </label></td>
+                                        <td><select
+                                            id="productType"
+                                            name="productType"
+                                            value={inputs.productType ? inputs.productType : "Type Switcher"}
+                                            onChange={handleChange}>
+                                            <option disabled>Type Switcher</option>
+                                            <option value="book">Book</option>
+                                            <option value="dvd">DVD</option>
+                                            <option value="furniture">Furniture</option>
+                                        </select></td>
+                                    </tr>
+
+                                    {additionalFields()}
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </form>
+                </div>
+            </>
+        )
     }
 
-    const additionalFields = () => {
-        if (inputs.productType === 'book') {
-            return (
-                <>
-                    <tr>
-                        <td><label htmlFor="weight">Weight (KG) </label></td>
-                        <td><input
-                            type="number"
-                            id="weight"
-                            name="weight"
-                            onChange={handleChange}
-                            required /></td>
-                    </tr>
-                    <tr><td colSpan={2}><strong className='fields'>Please provide the weight of the book.</strong></td></tr>
-                </>
-            );
-        } else if (inputs.productType === 'dvd') {
-            return (
-                <>
-                    <tr>
-                        <td><label htmlFor="size">Size (MB) </label></td>
-                        <td><input
-                            type="number"
-                            id="size"
-                            name="size"
-                            onChange={handleChange}
-                            required /></td>
-                    </tr>
-                    <tr><td colSpan={2}><strong className='fields'>Please provide the size(MB) of the DVD.</strong></td></tr>
-                </>
-            );
-        } else if (inputs.productType === 'furniture') {
-            return (
-                <>
-                    <tr>
-                        <td><label htmlFor="weight">Height (CM) </label></td>
-                        <td><input
-                            type="number"
-                            id="height"
-                            name="height"
-                            onChange={handleChange}
-                            required /></td>
-                    </tr>
-                    <tr>
-                        <td><label htmlFor="weight">Width (CM) </label></td>
-                        <td><input
-                            type="number"
-                            id="width"
-                            name="width"
-                            onChange={handleChange}
-                            required /></td>
-                    </tr>
-                    <tr>
-                        <td><label htmlFor="weight">Length (CM) </label></td>
-                        <td><input
-                            type="number"
-                            id="length"
-                            name="length"
-                            onChange={handleChange}
-                            required /></td>
-                    </tr>
-                    <tr><td colSpan={2}><strong className='fields'>Please provide the dimensions(CM) of the furniture.</strong></td></tr>
-                </>
-            );
-        } else { return null; }
-    }
-
-    return (
-        <>
-            <div className='form'>
-                <form id="product_form" method='post' onSubmit={handleSubmit}>
-                    <Header title={"Product Add"} />
-                    <div className='wrapper'>
-                        <table cellSpacing={10} className='product-add-form'>
-                            <tbody>
-                                <tr>
-                                    <td><label htmlFor="sku">SKU </label></td>
-                                    <td><input
-                                        type="text"
-                                        maxLength="24"
-                                        name="sku"
-                                        id="sku"
-                                        onChange={handleChange}
-                                        required /></td>
-                                </tr>
-                                <tr>
-                                    <td><label htmlFor="name">Name </label></td>
-                                    <td><input
-                                        type="text"
-                                        maxLength="24"
-                                        name="name"
-                                        id="name"
-                                        onChange={handleChange}
-                                        required /></td>
-                                </tr>
-                                <tr>
-                                    <td><label htmlFor="price">Price ($) </label></td>
-                                    <td><input
-                                        type="number"
-                                        maxLength="24"
-                                        name="price"
-                                        id="price"
-                                        onChange={handleChange}
-                                        required /></td>
-                                </tr>
-                                <tr>
-                                    <td><label htmlFor="productType">Type Switcher </label></td>
-                                    <td><select
-                                        id="productType"
-                                        name="productType"
-                                        value={inputs.productType ? inputs.productType : "Type Switcher"}
-                                        onChange={handleChange}>
-                                        <option disabled>Type Switcher</option>
-                                        <option value="book">Book</option>
-                                        <option value="dvd">DVD</option>
-                                        <option value="furniture">Furniture</option>
-                                    </select></td>
-                                </tr>
-
-                                {additionalFields()}
-
-                            </tbody>
-                        </table>
-                    </div>
-                </form>
-            </div>
-        </>
-    )
-}
-
-export default ProductAddForm
+    export default ProductAddForm
